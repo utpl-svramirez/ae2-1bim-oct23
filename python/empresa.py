618b53b214e7dff5dd2c4319886bef04028257b8
utpl-svramirez <svramirez@utpl.edu.ec>
2023-11-04 20:40:56 -0500

    actualizacion de clase empresa

--- python/empresa.py
+++ python/empresa.py
@@ -1,14 +1,14 @@
 class empresa:
-    def __init__(self, nombre, sector, cant_sucursales):
+    def __init__(self, nombre, sector_economico, cant_sucursales):
         self.nombre = nombre
-        self.sector = sector
+        self.sector_economico = sector_economico
         self.cant_sucursales = cant_sucursales
 
     def get_nombre(self):
         return self.nombre
 
-    def get_sector(self):
-        return self.sector
+    def get_sector_economico(self):
+        return self.get_sector_economico
 
     def get_cant_sucursales(self):
         return self.cant_sucursales
@@ -16,8 +16,8 @@ class empresa:
     def set_nombre(self, nombre_registro):
         self.nombre = nombre_registro
 
-    def set_sector(self, sector_registro):
-        self.sector = sector_registro
+    def set_sector_economico(self, sector_economico_registro):
+        self.sector_economico = sector_economico_registro
 
     def set_cant_sucursales(self, cant_sucursales_act):
         self.cant_sucursales = cant_sucursales_act
